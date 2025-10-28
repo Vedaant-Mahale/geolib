dd14f4e0fd7ca2cb1cc0f63ba8d42f0ff1e2e467
vedaant <vedaant.mahale@gmail.com>
2025-10-28 16:27:57 +0530

    Initial commit of Express backend and Vite frontend

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "https://geolib.onrender.com" }));
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
